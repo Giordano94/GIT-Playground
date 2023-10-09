36b3675f5de5b892775dfd5760f64e48adf46b9b
Giordano94 <giordano.mendes94@gmail.com>
2023-10-09 09:06:39 -0300

    feat: Change Form

--- src/Form/index.tsx
+++ src/Form/index.tsx
@@ -5,22 +5,28 @@ const Form: FC = () => {
   return (
     <div>
       <div>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Totam sed eos
-        dolorem quasi similique a repudiandae, quas pariatur quae laborum non
-        nesciunt, error vero praesentium alias doloribus? Eius, tempora
-        reprehenderit?
+        Lorem ipsum dulur sit amet consectetur, adipisicing elit. Totam sed eos
+        quae laborum non nas, error vero pater alias doloribus? Eius, tempora
+        qwertyu?
       </div>
       <h1>Form</h1>
+      <TextField label="A" />
+      <TextField label="Name" />
+      <TextField label="Name" />
+      <textarea name="" id="">
+        TextArea
+      </textarea>
       <TextField label="Name" />
       <TextField label="CPF" />
       <TextField label="Address" />
       <TextField label="Birth date" />
       <TextField label="Marital Status" />
       <div>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Recusandae
-        dolor officia iusto nisi voluptate ea quas asperiores, eius aliquam.
-        Cumque et veniam, dignissimos animi quod nihil commodi molestias
-        incidunt vero.
+        <div>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Recusandae
+        </div>
+        dolor officia iusto silimairum aliquam. Cumque et veniam, dignissimos
+        animi quod nihil commodi molestias incidunt vero.
       </div>
     </div>
   );
